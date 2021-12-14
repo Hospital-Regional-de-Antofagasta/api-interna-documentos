f7e3d792c3a6d078637651026dbbba9ccd0a650f
Pablo Fontecilla <pafonbu@gmail.com>
2021-12-14 15:31:49 -0300

    ADD: timestamps documentos

--- api/models/Documentos.js
+++ api/models/Documentos.js
@@ -3,15 +3,18 @@ const Schema = mongoose.Schema;
 
 const Documentos = mongoose.model(
   "documento",
-  new Schema({
-    correlativo: {type: Number, require: true},
-    identificadorDocumento: {type: String, require: true},
-    rutPaciente: {type: String, require: true},
-    tipo: {type: String, require: true},
-    fecha: {type: Date, require: true},
-    codigoEstablecimiento: {type: String, require: true},
-    nombreEstablecimiento: {type: String, require: true},
-  }),
+  new Schema(
+    {
+      correlativo: { type: Number, require: true },
+      identificadorDocumento: { type: String, require: true },
+      rutPaciente: { type: String, require: true },
+      tipo: { type: String, require: true },
+      fecha: { type: Date, require: true },
+      codigoEstablecimiento: { type: String, require: true },
+      nombreEstablecimiento: { type: String, require: true },
+    },
+    { timestamps: true }
+  ),
   "documentos"
 );
 
