f942f2e7eb664cd6029c9fbfbb6f84e1bfd9bcbd
Pablo Fontecilla <pafonbu@gmail.com>
2022-03-18 13:14:59 -0300

    console.logs de crear documentos old

--- api/controllers/documentosOldController.js
+++ api/controllers/documentosOldController.js
@@ -1,23 +1,36 @@
 const Documentos = require("../models/DocumentosOld");
 
 exports.create = async (req, res) => {
+  console.log("create old")
   try {
     const documentos = req.body;
+    console.log("documentos", documentos)
     if (Array.isArray(documentos)) {
+      console.log("array")
       for (let documento of documentos) {
         const documentoExistente = await Documentos.findOne({
           correlativo: documento.correlativo,
           tipo: documento.tipo,
         }).exec();
-        if (!documentoExistente) await Documentos.create(documento);
+        console.log("documentoExistente", documentoExistente)
+        if (!documentoExistente) {
+          await Documentos.create(documento);
+          console.log("documento creado")
+        }
       }
     } else {
+      console.log("object")
       const documentoExistente = await Documentos.findOne({
         correlativo: documentos.correlativo,
         tipo: documentos.tipo,
       }).exec();
-      if (!documentoExistente) await Documentos.create(documentos);
+      console.log("documentoExistente", documentoExistente)
+      if (!documentoExistente) {
+        await Documentos.create(documentos);
+        console.log("documento creado")
+      }
     }
+    console.log("create old finalizado")
     res.sendStatus(201);
   } catch (error) {
     console.log(`Documentos create: ${error.name} - ${error.message}`);
