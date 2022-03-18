82442d0322168d35b8ddd2412ef24d66b50c9172
Pablo Fontecilla <pafonbu@gmail.com>
2022-03-18 13:27:08 -0300

    FIX: nada

--- api/controllers/documentosOldController.js
+++ api/controllers/documentosOldController.js
@@ -4,32 +4,18 @@ exports.create = async (req, res) => {
   console.log("create old")
   try {
     const documentos = req.body;
-    console.log("documentos", documentos)
-    if (Array.isArray(documentos)) {
-      console.log("array")
-      for (let documento of documentos) {
-        const documentoExistente = await Documentos.findOne({
-          correlativo: documento.correlativo,
-          tipo: documento.tipo,
-        }).exec();
-        console.log("documentoExistente", documentoExistente)
-        if (!documentoExistente) {
-          await Documentos.create(documento);
-          console.log("documento creado")
-        }
-      }
-    } else {
-      console.log("object")
-      const documentoExistente = await Documentos.findOne({
-        correlativo: documentos.correlativo,
-        tipo: documentos.tipo,
-      }).exec();
-      console.log("documentoExistente", documentoExistente)
-      if (!documentoExistente) {
-        await Documentos.create(documentos);
-        console.log("documento creado")
-      }
-    }
+    console.log("documentos", documentos.length)
+    // for (let documento of documentos) {
+    //   const documentoExistente = await Documentos.findOne({
+    //     correlativo: documento.correlativo,
+    //     tipo: documento.tipo,
+    //   }).exec();
+    //   console.log("documentoExistente", !documentoExistente)
+    //   if (!documentoExistente) {
+    //     await Documentos.create(documento);
+    //     console.log("documento creado")
+    //   }
+    // }
     console.log("create old finalizado")
     res.sendStatus(201);
   } catch (error) {
