ddf58e5e30c228e9b47256a2ffd5f860ab512007
Pablo Fontecilla <pafonbu@gmail.com>
2022-03-18 14:05:35 -0300

    FIX: reagregar endpoint de crear documentos antiguo

--- api/controllers/documentosOldController.js
+++ api/controllers/documentosOldController.js
@@ -1,25 +1,19 @@
 const Documentos = require("../models/DocumentosOld");
 
 exports.create = async (req, res) => {
-  console.log("create old")
   try {
     const documentos = req.body;
-    console.log("documentos", documentos.length)
-    // for (let documento of documentos) {
-    //   const documentoExistente = await Documentos.findOne({
-    //     correlativo: documento.correlativo,
-    //     tipo: documento.tipo,
-    //   }).exec();
-    //   console.log("documentoExistente", !documentoExistente)
-    //   if (!documentoExistente) {
-    //     await Documentos.create(documento);
-    //     console.log("documento creado")
-    //   }
-    // }
-    console.log("create old finalizado")
+    for (let documento of documentos) {
+      const documentoExistente = await Documentos.findOne({
+        correlativo: documento.correlativo,
+        tipo: documento.tipo,
+      }).exec();
+      if (!documentoExistente) {
+        await Documentos.create(documento);
+      }
+    }
     res.sendStatus(201);
   } catch (error) {
-    console.log(`Documentos create: ${error.name} - ${error.message}`);
     res.status(500).send({
       respuesta: `Documentos create: ${error.name} - ${error.message}`,
     });
