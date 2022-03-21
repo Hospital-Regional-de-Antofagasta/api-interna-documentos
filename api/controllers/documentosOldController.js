eb1a17429347297a7951d88b649a372fdb8ec18a
Pablo Fontecilla <pafonbu@gmail.com>
2022-03-21 10:25:11 -0300

    FIX: documentos old not create existing document attempt 4

--- api/controllers/documentosOldController.js
+++ api/controllers/documentosOldController.js
@@ -1,21 +1,21 @@
 const Documentos = require("../models/DocumentosOld");
 
 exports.create = async (req, res) => {
-  console.log("Documentos old create");
+  console.log("Documentos old create", new Date());
   try {
     const documentos = req.body;
     console.log("Cantidad documentos", documentos.length);
     if (documentos.length === 0) {
-      console.log("Documentos old create finish");
+      console.log("Documentos old create finish", new Date());
       return res.sendStatus(201);
     }
-    console.log("Tipo documentos", documentos[0].tipo);
+    console.log("Tipo documentos", documentos[0].tipo, new Date());
     const filter = documentos[0].tipo === "DAU" ? "DAU" : "EPICRISIS";
     const documentosBD = await Documentos.find({ filter }).exec();
 
     let cont = 1;
     for (let documento of documentos) {
-      console.log("Cantidad documentos revisados", cont)
+      console.log("Cantidad documentos revisados", cont, new Date())
       cont ++;
       if (
         !documentosBD.find(
@@ -25,13 +25,13 @@ exports.create = async (req, res) => {
         )
       ) {
         await Documentos.create(documento);
-        console.log("Documento creado");
+        console.log("Documento creado", new Date());
       }
     }
-    console.log("Documentos old create finish");
+    console.log("Documentos old create finish", new Date());
     return res.sendStatus(201);
   } catch (error) {
-    console.log(`Error documentos old create: ${error.name} - ${error.message}`)
+    console.log(`Error documentos old create: ${error.name} - ${error.message}`, new Date())
     res.status(500).send({
       respuesta: `Documentos create: ${error.name} - ${error.message}`,
     });
